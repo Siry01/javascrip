3c4e205e838254459bf0355d6d3f530c604dd78e
siry01 <sirypacheco01@gmail.com>
2026-02-21 14:08:55 -0400

    alert

---
+++ js/alert.js
@@ -0,0 +1,7 @@
+"use strict";
+var n1 = parseInt(prompt("Introduce un numero 1", 0));
+var n2 = parseInt(prompt("Introduce un numero 2", 0));
+
+console.log(n1,n2);
+console.log("La suma es " + (n1 + n2));
+
